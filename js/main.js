9ed5d0cd18437f4ace8879ac4081a4019b238d09
Your Name <you@example.com>
2022-04-18 14:38:09 +0300

    message

--- js/main.js
+++ js/main.js
@@ -32,3 +32,58 @@ const swiperRange = new Swiper('.range__swiper', {
 		prevEl: '.range__prev',
 	},
 });
+
+// validate
+
+//Name
+const inpEmail = document.querySelectorAll('.form__input')
+const errEmail = document.querySelectorAll('.form__input-container')
+
+var regEmail = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+let count = 0
+
+document.querySelectorAll('.form__btn').forEach(function (item,index){
+	item.addEventListener('click' , function(e){
+			e.preventDefault()
+
+			errEmail.forEach(function (item){
+				item.classList.remove('is-valid')
+				item.classList.remove('is-invalid')
+			})
+
+			if(!isEmail (regEmail, inpEmail[index].value) || inpEmail[index].value.length <= 3) {
+				notValid(errEmail[index])
+				count--
+			} else {
+				valid(errEmail[index])
+				count++
+			}
+
+			if(count < 1) {
+				count = 0
+			}
+
+			if(count === 0) {
+				count = 0
+			}
+			inpEmail.forEach(function (item){
+				item.value = ''
+			})
+	})
+})
+
+
+function isEmail (regex, inp) {
+	return regex.test(inp)
+}
+
+function notValid (inp) {
+	inp.classList.remove('is-valid')
+	inp.classList.add('is-invalid')
+}
+
+function valid (inp) {
+	inp.classList.remove('is-invalid')
+	inp.classList.add('is-valid')
+}
