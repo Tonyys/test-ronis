056594c58a336c25a61a0bef58cb72da7f25e3d0
Your Name <you@example.com>
2022-04-21 11:13:02 +0300

    message

--- js/main.js
+++ js/main.js
@@ -1,75 +1,75 @@
 // swiper
-const swiperRange = new Swiper('.range__swiper', {
-	loop: true,
-	centeredSlides: true,
-	spaceBetween: 100,
-	autoplay: {
-		delay: 4000,
-	},
-	speed: 800,
-	pagination: {
-		el: '.range__pagination',
-		type: 'fraction'
-	},
-	navigation: {
-		nextEl: '.range__next',
-		prevEl: '.range__prev',
-	},
+new Swiper('.range__swiper', {
+    loop: true,
+    centeredSlides: true,
+    spaceBetween: 100,
+    autoplay: {
+        delay: 4000,
+    },
+    speed: 800,
+    pagination: {
+        el: '.range__pagination',
+        type: 'fraction'
+    },
+    navigation: {
+        nextEl: '.range__next',
+        prevEl: '.range__prev',
+    },
 });
 
 // Nav scrollbar
 
 document.querySelectorAll('.intro__nav-item').forEach( (currentItem) => {
-	const btnItems = document.querySelectorAll('.intro__nav-item')
-	const itemAttr = currentItem.getAttribute("data-scroll")
-	const currentSection = document.querySelector(itemAttr)
+const btnItems = document.querySelectorAll('.intro__nav-item')
+const itemAttr = currentItem.getAttribute("data-scroll")
+const currentSection = document.querySelector(itemAttr)
 
 
-	function throttle (func, ms) {
-		let locked = false
-		return function() {
-			if (locked) return
+function throttle (func, ms) {
+    let locked = false
+    return function() {
+        if (locked) return
 
-			const context = this
-			const args = arguments
+        const context = this
+        const args = arguments
 
-			locked = true
+        locked = true
 
-			setTimeout(() => {
-				func.apply(context, args)
-				locked = false
-			}, ms)
-		}
-	}
-	window.addEventListener('scroll', throttle(scroll,200))
+        setTimeout(() => {
+            func.apply(context, args)
+            locked = false
+        }, ms)
+    }
+}
+window.addEventListener('scroll', throttle(scroll,200))
 
-	function scroll (){
-		console.log(1111)
-		const windowHeight = window.screen.height * 0.50
-		if(window.scrollY >= currentSection.offsetTop - windowHeight){
-			btnItems.forEach( (item) =>{
-				item.classList.remove('active')
-			})
-			currentItem.classList.add('active')
-		} else {
-			currentItem.classList.remove('active')
-		}
- 	}
+function scroll (){
+    console.log(1111)
+    const windowHeight = window.screen.height * 0.50
+    if(window.scrollY >= currentSection.offsetTop - windowHeight){
+        btnItems.forEach( (item) =>{
+            item.classList.remove('active')
+        })
+        currentItem.classList.add('active')
+    } else {
+        currentItem.classList.remove('active')
+    }
+}
 
-	currentItem.addEventListener('click', (e) => {
-		
-		btnItems.forEach((item) => {
-			item.classList.remove('active')
-		})
+currentItem.addEventListener('click', (e) => {
 
-		currentItem.classList.add('active')
-		scrollTo(currentSection)
-	})
+    btnItems.forEach((item) => {
+        item.classList.remove('active')
+    })
+
+    currentItem.classList.add('active')
+    scrollTo(currentSection)
+})
 })
 function scrollTo(element) {
-	window.scroll({
-		left: 0,
-		top: element.offsetTop,
-		behavior: 'smooth'
-	})
+window.scroll({
+    left: 0,
+    top: element.offsetTop,
+    behavior: 'smooth'
+})
 }
