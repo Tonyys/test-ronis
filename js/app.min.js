b7c56bdadfab7c83aa73f69d90541c077d6b8d3f
Your Name <you@example.com>
2022-04-21 16:34:18 +0300

    message

--- js/app.min.js
+++ js/app.min.js
@@ -18,57 +18,63 @@ new Swiper('.range__swiper', {
 });
 
 // Nav scrollbar
-
-document.querySelectorAll('.intro__nav-item').forEach( (currentItem) => {
-    const btnItems = document.querySelectorAll('.intro__nav-item')
-    const itemAttr = currentItem.getAttribute("data-scroll")
-    const currentSection = document.querySelector(itemAttr)
+(function (){
+    const arrAttr = []
     const windowHeight = window.screen.height * 0.50
+    const btnItems = document.querySelectorAll('.intro__nav-item[data-scroll]')
+    btnItems.forEach(function (el){
+        arrAttr.push(el)
+    })
+    arrAttr.forEach(function (currentItem){
+        const currentAttr = currentItem.getAttribute('data-scroll')
+        const currentSection = document.querySelector(currentAttr)
 
-    window.addEventListener('scroll', throttle(scroll,200))
-
-    function throttle (func, ms) {
-        let locked = false
-        return function() {
-            if (locked) return
-
-            const context = this
-            const args = arguments
-
-            locked = true
-
-            setTimeout(() => {
-                func.apply(context, args)
-                locked = false
-            }, ms)
+        window.addEventListener('scroll', scroll)
+        function scroll (){
+            if(window.scrollY >= currentSection.offsetTop - windowHeight){
+                btnItems.forEach( (item) =>{
+                    item.classList.remove('active')
+                })
+                currentItem.classList.add('active')
+            } else {
+                currentItem.classList.remove('active')
+            }
         }
-    }
-    function scroll (){
-        console.log(1111)
-        if(window.scrollY >= currentSection.offsetTop - windowHeight){
-            btnItems.forEach( (item) =>{
+
+        currentItem.addEventListener('click', (e) => {
+
+            btnItems.forEach((item) => {
                 item.classList.remove('active')
             })
+
             currentItem.classList.add('active')
-        } else {
-            currentItem.classList.remove('active')
-        }
-    }
-
-    currentItem.addEventListener('click', (e) => {
-
-        btnItems.forEach((item) => {
-            item.classList.remove('active')
+            scrollTo(currentSection)
         })
-
-        currentItem.classList.add('active')
-        scrollTo(currentSection)
     })
-})
-function scrollTo(element) {
-window.scroll({
-    left: 0,
-    top: element.offsetTop,
-    behavior: 'smooth'
-})
-}
+
+    // function throttle (func, ms) {
+        //     let locked = false
+        //     return function() {
+        //         if (locked) return
+        //
+        //         const context = this
+        //         const args = arguments
+        //
+        //         locked = true
+        //
+        //         setTimeout(() => {
+        //             func.apply(context, args)
+        //             locked = false
+        //         }, ms)
+        //     }
+        // }
+
+
+    function scrollTo(element) {
+        window.scroll({
+        left: 0,
+        top: element.offsetTop,
+        behavior: 'smooth'
+     })
+    }
+})()
